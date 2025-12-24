0d8ac4d0af80f5bee98ba41306de1504b9130413
Roman <roman.buniak@kgemt.org.ua>
2025-12-25 00:13:40 +0200

    Add time message functionality

--- MessagePrinter.java
+++ MessagePrinter.java
@@ -13,12 +13,18 @@ public static String getWelcomeMessage() {
     public static void main(String[] args) {
     System.out.println(getWelcomeMessage());
     System.out.println(getDateMessage());
+    System.out.println(getTimeMessage());
     System.out.println("Learning remote repositories!");
     System.out.println(getGoodbyeMessage());
 }
 
+
 public static String getGoodbyeMessage() {
     return "Thank you for using MessagePrinter!";
 }
+public static String getTimeMessage() {
+    return "Current time: " + java.time.LocalTime.now();
+}
+
 
 }
